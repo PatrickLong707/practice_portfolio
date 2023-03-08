15c4077a9cafda21961a796d5f4b89b98ab9b058
PatrickLong707 <125424139+PatrickLong707@users.noreply.github.com>
2023-03-07 17:45:46 -0800

    start my portfolio

---
+++ starter.js
@@ -0,0 +1,11 @@
+console.log('Ahoy!');
+console.log('Pat!');
+const usersAge = 111;
+
+const typeOfDrink = 'strong';
+
+switch (typeOfDrink) 
+    Case 'fruity':
+        console.log('strawberry daiquiri')
+        
+
